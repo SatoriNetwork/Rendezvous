ef898fab22c43cb509bd0e0c82db4afc109739aa
Jordan Miller <Jordan.kay@gmail.com>
2023-11-24 18:32:51 -0700

    update

--- tests/manual/udp.py
+++ tests/manual/udp.py
@@ -5,50 +5,67 @@
 #       python3 udp.py <other ip address> 5003 5002 True
 #   at the same time.
 # now they can talk to each other directly.
-# docker run --rm -it --privileged -p 5002:5002/udp -p 5003:5003/udp -v .:/udp python:slim bash
-# python /udp/udp.py 45.83.220.218 5003 5002 True
 
-# docker run --rm -it --privileged --net=host -v .:/udp python:slim bash
-# apt-get update; apt-get install curl -y
-# curl https://satorinet.io/ip/
+# to test within a container:
+# docker run --rm -it --privileged -p 5002:5002/udp -p 5003:5003/udp -v .:/udp python:slim bash
+# python /udp/udp.py <remoteIp> 5003 5002 True
+
+# how to get the public ip address:
+# docker run --rm -it --privileged --net=host --cap-add NET_ADMIN -v .:/udp python:slim bash
+# apt-get update; apt-get install curl -y; curl https://satorinet.io/ip/
 
 import sys
 import socket
 import threading
 
 
-def listen(sock: socket.socket):
-    while True:
-        data, addr = sock.recvfrom(1024)
-        # data = sock.recv(1024)
-        print(f'\rpeer ({addr}): {data.decode()}\n> ', end='')
+class SimpleUDP():
+    def __init__(self, remoteIp, remotePort=5002, localPort=5001, myLocalIsTheirRemote=False):
+        self.remoteIp = remoteIp
+        self.remotePort = int(remotePort)
+        self.originalRemotePort = int(remotePort)
+        self.localPort = int(localPort)
+        self.myLocalIsTheirRemote = myLocalIsTheirRemote
+        self.sock: socket.socket
+        self.sock2: socket.socket
+        self.run()
 
+    def listen(self):
+        while True:
+            # data = sock.recv(1024)
+            data, addr = self.sock.recvfrom(1024)
+            if addr[1] != remotePort:
+                remotePort = int(addr[1])
+                print(f'\rremote port: {remotePort}\n', end='')
+            print(f'\rpeer ({addr}): {data.decode()}\n> ', end='')
 
-def run(remoteIp, remotePort=5002, localPort=5001, myLocalIsTheirRemote=False):
-    localPort = int(localPort)
-    remotePort = int(remotePort)
-    print('\ngot peer')
-    print('  ip:          {}'.format(remoteIp))
-    print('  localPort: {}'.format(localPort))
-    print('  repotePort:   {}\n'.format(remotePort))
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(('0.0.0.0', localPort))
-    sock.sendto(b'0', (remoteIp, remotePort))
-    listener = threading.Thread(target=listen, args=[sock], daemon=True)
-    listener.start()
-    if not myLocalIsTheirRemote:
-        sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock2.bind(('0.0.0.0', remotePort))
-    while True:
-        msg = input('> ')
-        if not myLocalIsTheirRemote:
-            sock2.sendto(msg.encode(), (remoteIp, localPort))
-        else:
-            sock.sendto(msg.encode(), (remoteIp, remotePort))
+    def run(self):
+        print('\ngot peer')
+        print(f'  remoteip:   {self.remoteIp}')
+        print(f'  repotePort: {self.remotePort}')
+        print(f'  localPort:  {self.localPort}')
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(('0.0.0.0', self.localPort))
+        self.sock.sendto(b'0', (self.remoteIp, self.remotePort))
+        listener = threading.Thread(target=self.listen, daemon=True)
+        listener.start()
+        if not self.myLocalIsTheirRemote:
+            self.sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            self.sock2.bind(('0.0.0.0', self.remotePort))
+
+    def send(self):
+        while True:
+            msg = input('> ')
+            if not self.myLocalIsTheirRemote:
+                self.sock2.sendto(
+                    msg.encode(), (self.remoteIp, self.localPort))
+            else:
+                self.sock.sendto(
+                    msg.encode(), (self.remoteIp, self.remotePort))
 
 
 if __name__ == '__main__' and len(sys.argv) > 4:
-    run(
+    SimpleUDP(
         remoteIp=sys.argv[1],
         remotePort=sys.argv[2],
         localPort=sys.argv[3],
